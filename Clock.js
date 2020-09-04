6e1968088fe9555ca1534046e2a9fb6ae4eadb15
Vibhu3 <65866042+Vibhu3@users.noreply.github.com>
2020-09-04 13:24:40 -0400

    Add files via upload

---
+++ Clock.js
@@ -0,0 +1,12 @@
+class Clock {
+    constructor(x,y,width,height) {
+       
+        this.image = loadImage("clock.png");
+      }
+      display(){
+        push();
+        imageMode(CENTER);
+        image(this.image, 0, 0, 493,493);
+        pop();
+      }
+    };
